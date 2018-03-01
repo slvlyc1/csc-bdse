8c93258c014591cc3f06ce3d3d37ee97d05c1499
koalena <infypavlin@gmail.com>
2018-03-01 23:40:07 +0300

    Added ability to start/stop node

--- bdse-kvnode/src/main/java/ru/csc/bdse/kv/MongoDBKeyValueApi.java
+++ bdse-kvnode/src/main/java/ru/csc/bdse/kv/MongoDBKeyValueApi.java
@@ -15,11 +15,14 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static ru.csc.bdse.kv.NodeStatus.*;
+
 public class MongoDBKeyValueApi implements KeyValueApi {
 
     @Autowired
     private MongoOperations mongoTemplate;
     private final String nodeName;
+    private volatile NodeStatus nodeStatus;
 
     private static final Logger log = LoggerFactory.getLogger(MongoDBKeyValueApi.class);
 
@@ -27,12 +30,14 @@ public class MongoDBKeyValueApi implements KeyValueApi {
     public MongoDBKeyValueApi(String nodeName) {
         Require.nonEmpty(nodeName, "empty name");
         this.nodeName = nodeName;
+        this.nodeStatus = UP;
     }
 
     @Override
     public void put(String key, byte[] value) {
         Require.nonEmpty(key, "empty key");
         Require.nonNull(value, "null value");
+        if (nodeStatus.equals(DOWN)) throw new IllegalStateException(String.format("Node '%s' is down", nodeName));
 
         Query query = new Query();
         query.addCriteria(Criteria.where("key").is(key));
@@ -46,6 +51,7 @@ public class MongoDBKeyValueApi implements KeyValueApi {
     @Override
     public Optional<byte[]> get(String key) {
         Require.nonEmpty(key, "empty key");
+        if (nodeStatus.equals(DOWN)) throw new IllegalStateException(String.format("Node '%s' is down", nodeName));
 
         Query byKey = new Query().addCriteria(Criteria.where("key").is(key));
         KeyValue result = mongoTemplate.findOne(byKey, KeyValue.class);
@@ -56,6 +62,7 @@ public class MongoDBKeyValueApi implements KeyValueApi {
     @Override
     public Set<String> getKeys(String prefix) {
         Require.nonNull(prefix, "null prefix");
+        if (nodeStatus.equals(DOWN)) throw new IllegalStateException(String.format("Node '%s' is down", nodeName));
 
         Query query = new Query();
         query.addCriteria(Criteria.where("key").regex(String.format("^%s", prefix)));
@@ -67,17 +74,34 @@ public class MongoDBKeyValueApi implements KeyValueApi {
 
     @Override
     public void delete(String key) {
+        Require.nonEmpty(key, "empty key");
+        if (nodeStatus.equals(DOWN)) throw new IllegalStateException(String.format("Node '%s' is down", nodeName));
+
         Query query = new Query(Criteria.where("key").is(key));
         mongoTemplate.remove(query, KeyValue.class);
     }
 
     @Override
     public Set<NodeInfo> getInfo() {
-        return Collections.singleton(new NodeInfo(nodeName, NodeStatus.UP));
+        return Collections.singleton(new NodeInfo(nodeName, nodeStatus));
     }
 
     @Override
     public void action(String node, NodeAction action) {
-        throw new RuntimeException("action not implemented now");
+        Require.nonEmpty(node, "empty node name");
+        Require.nonNull(action, "empty action");
+
+        if (!node.equals(nodeName)) return;
+
+        switch (action) {
+            case UP:
+                nodeStatus = UP;
+                break;
+            case DOWN:
+                nodeStatus = DOWN;
+                break;
+            default:
+                throw new IllegalArgumentException(String.format("Unknown NodeAction '%s'", action));
+        }
     }
 }
